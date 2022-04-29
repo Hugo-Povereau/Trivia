854f2407e14fc41bfaf6fff1bb27960011a6de0b
Povereau Hugo <hugo.povereau@maincare.fr>
2022-04-29 18:49:00 +0200

    première review

--- trivia.py
+++ trivia.py
@@ -3,7 +3,7 @@
 class Game:
     def __init__(self):
         self.players = []
-        self.places = [0] * 6
+        self.places = [0] * 6 #pourquoi 6? si limité éviter crash. erreur perso?
         self.purses = [0] * 6
         self.in_penalty_box = [0] * 6
 
@@ -15,26 +15,26 @@ class Game:
         self.current_player = 0
         self.is_getting_out_of_penalty_box = False
 
-        for i in range(50):
+        for i in range(50): #dépasse 50?
             self.pop_questions.append("Pop Question %s" % i)
             self.science_questions.append("Science Question %s" % i)
             self.sports_questions.append("Sports Question %s" % i)
             self.rock_questions.append(self.create_rock_question(i))
 
-    def create_rock_question(self, index):
+    def create_rock_question(self, index):  #pourquoi différent?
         return "Rock Question %s" % index
 
-    def is_playable(self):
+    def is_playable(self): #marche pas??
         return self.how_many_players >= 2
 
     def add(self, player_name):
         self.players.append(player_name)
         self.places[self.how_many_players] = 0
         self.purses[self.how_many_players] = 0
-        self.in_penalty_box[self.how_many_players] = False
+        self.in_penalty_box[self.how_many_players] = False # à voir si vrmt bligatoire
 
         print(player_name + " was added")
-        print("They are player number %s" % len(self.players))
+        print("They are player number %s" % len(self.players)) #phrase inccorecte? they are->is?
 
         return True
 
@@ -47,13 +47,13 @@ class Game:
         print("They have rolled a %s" % roll)
 
         if self.in_penalty_box[self.current_player]:
-            if roll % 2 != 0:
+            if roll % 2 != 0: #impair pour sortir!
                 self.is_getting_out_of_penalty_box = True
 
                 print("%s is getting out of the penalty box" % self.players[self.current_player])
                 self.places[self.current_player] = self.places[self.current_player] + roll
-                if self.places[self.current_player] > 11:
-                    self.places[self.current_player] = self.places[self.current_player] - 12
+                if self.places[self.current_player] > 11: #plateau taille 12, variabiliser?
+                    self.places[self.current_player] = self.places[self.current_player] - 12 # si supérieur à 12???
 
                 print(self.players[self.current_player] + \
                             '\'s new location is ' + \
@@ -81,7 +81,7 @@ class Game:
         if self._current_category == 'Rock': print(self.rock_questions.pop(0))
 
     @property
-    def _current_category(self):
+    def _current_category(self): #calcul avec mod
         if self.places[self.current_player] == 0: return 'Pop'
         if self.places[self.current_player] == 4: return 'Pop'
         if self.places[self.current_player] == 8: return 'Pop'
@@ -95,23 +95,23 @@ class Game:
 
     def was_correctly_answered(self):
         if self.in_penalty_box[self.current_player]:
-            if self.is_getting_out_of_penalty_box:
+            if self.is_getting_out_of_penalty_box: #nécessaire? éviter avec return dans autre fonction?
                 print('Answer was correct!!!!')
                 self.purses[self.current_player] += 1
                 print(self.players[self.current_player] + \
                     ' now has ' + \
                     str(self.purses[self.current_player]) + \
-                    ' Gold Coins.')
+                    ' Gold Coins.') #écriture plus propre?
 
                 winner = self._did_player_win()
                 self.current_player += 1
                 if self.current_player == len(self.players): self.current_player = 0
 
-                return winner
-            else:
-                self.current_player += 1
-                if self.current_player == len(self.players): self.current_player = 0
-                return True
+                return winner #répétition
+            else: #nécessaire?
+                self.current_player += 1 #répétition
+                if self.current_player == len(self.players): self.current_player = 0 #répétition
+                return True #???
 
 
 
@@ -125,10 +125,10 @@ class Game:
                 ' Gold Coins.')
 
             winner = self._did_player_win()
-            self.current_player += 1
-            if self.current_player == len(self.players): self.current_player = 0
+            self.current_player += 1 # répétition
+            if self.current_player == len(self.players): self.current_player = 0 #répétition
 
-            return winner
+            return winner #répétition
 
     def wrong_answer(self):
         print('Question was incorrectly answered')
@@ -136,14 +136,14 @@ class Game:
         self.in_penalty_box[self.current_player] = True
 
         self.current_player += 1
-        if self.current_player == len(self.players): self.current_player = 0
+        if self.current_player == len(self.players): self.current_player = 0 #répétition (voir fun au dessus)
         return True
 
     def _did_player_win(self):
-        return not (self.purses[self.current_player] == 6)
+        return not (self.purses[self.current_player] == 6) # premier à 6coins
 
 
-from random import randrange
+from random import randrange #monter
 
 if __name__ == '__main__':
     not_a_winner = False
@@ -153,6 +153,9 @@ if __name__ == '__main__':
     game.add('Chet')
     game.add('Pat')
     game.add('Sue')
+    game.add('zaezaea')
+    game.add('zaeaze')
+    #game.add('zaeza') # limité 5j échoue si plus
 
     while True:
         game.roll(randrange(5) + 1)
